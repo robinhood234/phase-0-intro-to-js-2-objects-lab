8a7c9952fa61af4166702f2b0a8e4d9e2d561d89
robin nganga smith <saintofcookies.2021@gmail.com>
2023-10-06 11:15:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+  name:'Robin',
+  streetAddress:'Naivasha',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  return {...employee, name:'Sam',streetAddress:'11 Broadway'}
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  
+  employee.name = 'Sam'
+  employee.streetAddress = '12 Broadway'
+return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee.name};
+  delete newEmployee[0];
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  employee.name
+  delete employee.name;
+  return employee;
+}
